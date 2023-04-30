85dd4ee6803d800aa11089c3577e3a9eb738d42d
Lucas Bandeira <lucasbandeirai40@gmail.com>
2023-04-30 09:50:02 -0300

    Model

--- Template/EscolaTemplate/src/Model/Aluno.java
+++ Template/EscolaTemplate/src/Model/Aluno.java
@@ -1,5 +1,4 @@
 package Model;
-
 public class Aluno {
     private int id;
     private String nome;
--- Template/EscolaTemplate/src/Model/AlunoTemplate.java
+++ Template/EscolaTemplate/src/Model/AlunoTemplate.java
@@ -1,13 +1,15 @@
 package Model;
 
 public abstract class AlunoTemplate {
-    public final String processarSituacao(Aluno aluno){
+    public final String processarSituacao(Aluno aluno) {
         double notaFinal = calcularNotaFinal(aluno);
         int frequencia = aluno.getFrequencia();
         return determinarSituacao(notaFinal, frequencia);
 
     }
+
     protected abstract double calcularNotaFinal(Aluno aluno);
+
     protected abstract String determinarSituacao(double notaFinal, int frequencia);
 
 }
